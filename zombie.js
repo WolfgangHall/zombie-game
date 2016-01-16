75f6a910e8fb905cb55420150f509f53969aeac2
Johall <johnalexander.w.hall@gmail.com>
2016-01-16 13:47:08 -0500

    added working prompt

--- zombie.js
+++ zombie.js
@@ -1,49 +1,47 @@
 var prompt = require("request");
 
-var userHealth = 100;
-var foodRations = 50;
-var axeDamage = 4;
-var fifthSense = 0;
+ var prompt = require("prompt");
+ 
+  // 
+  // Setting these properties customizes the prompt. 
+  // 
+  prompt.message = "Question!";
+  prompt.delimiter = "><".green;
+ 
+  prompt.start();
+ 
+  prompt.get({
+    properties: {
+      name: {
+        description: "Choose a number between 1-10".black
+      }
+    }
+  }, function (err, result) {
+    console.log("You picked: ".blue + result.name.blue);
+  });
+// var userHealth = 100;
 
-var zombieHP = 4;
-var zombieDamage = 5;
+// var zombieHP = 20;
+// var zombieDamage = 5;
 
-var daysPassed =;
+// var daysPassed = 1;
 
-var hitZombieChance = Math.random() > 0.5;
-var encounterZombieChance = Math.random() > 0.93;
-var findFoodChance;
-var findGunChance;
+// var hitZombieChance = Math.random() > 0.5;
+
+// var findFoodChance;
+// var findGunChance;
 
 
-if (encounterZombieChance === true) {
-   foodRations --;
-}
+// if (encounterZombieChance === true) {
+//    foodRations --;
+// }
 
-if (foodRations === 0 && userHealth === 0) {
-   console.log("Wrecked");
-   return;
-}
+// if (foodRations === 0 && userHealth === 0) {
+//    console.log("Wrecked");
+//    return;
+// }
 
-if (fifthSense < 5) {
-   var findFoodChance = Math.random() > 0.99;
-   var findGunChance = Math.random() > 0.99;
-}
 
-if (fifthSense < 7) {
-   var findFoodChance = Math.random() > 0.95;
-   var findGunChance = Math.random() > 0.95;
-}
-
-if (fifthSense < 10) {
-   var findFoodChance = Math.random() > 0.93;
-   var findGunChance = Math.random() > 0.93;
-}
-
-if (fifthSense === 10) {
-   var findFoodChance = Math.random() > 0.90;
-   var findGunChance = Math.random() > 0.90;
-}
 
 
 
